cc9fcce3c8f1542f1c03e47c07f947f4713a26f4
siyeong <mandarin19@icloud.com>
2026-09-02 17:33:14 +0900

    RestTemplate 생성

--- demo/src/main/java/com/example/demo/global/config/RestTemplateConfig.java
+++ demo/src/main/java/com/example/demo/global/config/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.domain.fortune.config;
+package com.example.demo.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
