1801a0f737d73f3ebc54f79758762965f198673f
bsh <qotngus72@naver.com>
2026-09-07 10:31:43 +0900

    스웨거 테스트 추가

--- demo/src/main/java/com/example/demo/domain/user/dto/UserGameCreateRequest.java
+++ demo/src/main/java/com/example/demo/domain/user/dto/UserGameCreateRequest.java
@@ -2,6 +2,7 @@ package com.example.demo.domain.user.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,5 +12,6 @@ public class UserGameCreateRequest {
 
     @NotNull(message = "게임 ID는 필수입니다.")
     @Positive(message = "게임 ID는 양수여야 합니다.")
+    @Schema(description = "game 테이블에 존재하는 게임 ID", example = "1", minimum = "1", requiredMode = Schema.RequiredMode.REQUIRED)
     private Long gameId;
 }
